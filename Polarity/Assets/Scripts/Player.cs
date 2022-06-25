c439de08f7c7911dedacde784b12da3503b6e09d
Galaxian <manognachamarthi@gmail.com>
2022-06-25 21:41:55 +0530

    Main System is almost done
    Still have to do
    * enemy attack
    * our attack
    * enemy and obstacles initialization.

--- Polarity/Assets/Scripts/Player.cs
+++ Polarity/Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     //Variables
     public float jumpSpeed;
-    public float polarity;
+    private float polarity = 1;
     private Rigidbody rb;
 
     public bool isGrounded; 
@@ -21,15 +21,37 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded== true)
         {
-            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpSpeed * polarity, ForceMode.Impulse);
             isGrounded = false;
             Debug.Log("is jumping");
         }
+        
+        Polarity();
     }
 
     void OnCollisionStay(Collision collision)
     {
         isGrounded = true;
+    }
+
+    public void Polarity()
+    {
+        //get input for polarity change
+        //invert gravity when input is recieved
+        //make it so that charged polarity can make you attack enemy.(it takes about 5 seconds to charge)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            polarity *= -1;
+            Physics.gravity = new Vector3(0, 9.81f * (-polarity),0);
+            Debug.Log("Polarity is reversed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            //becomes able to hit enemies.
+            Debug.Log("I am right clicking");
+        }
+
     }
 
 }
