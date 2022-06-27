281f0b2ca0c57333891edd88dc58566e52b7631d
Galaxian <manognachamarthi@gmail.com>
2022-06-27 10:40:51 +0530

    Added obstacle spawner

--- Polarity/Assets/Scripts/Enemy.cs
+++ Polarity/Assets/Scripts/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //variables
+    public GameObject obstacle;
+    private float timer;
+    public float max_time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject new_Obstacle = Instantiate(obstacle);
+        new_Obstacle.transform.position = transform.position + new Vector3(Random.Range(-10, 10), 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (timer > max_time)
+        {
+            GameObject new_Obstacle = Instantiate(obstacle);
+            new_Obstacle.transform.position = transform.position + new Vector3(Random.Range(-10, 10), 0, 0);
+            Destroy(new_Obstacle, 7);
+            timer = 0;
+        }
+        timer += Time.deltaTime ;
     }
 }
---
+++ Polarity/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    //variables
+    public float obstacleSpeed;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * obstacleSpeed * Time.deltaTime;
+    }
+}
